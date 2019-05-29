eda92b2625198623a0c6d3159b20731389b0c4b4
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-05-29 16:12:47 -0700

    Day 1 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+from art import art
 
 # Declare all the rooms
 
@@ -38,6 +40,10 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+inputName = input('\x1b[1;33;40m' + "Please player's name: " + '\x1b[0m')
+player = Player(inputName, room['outside'])
+print('\x1b[0;36;40m' + "\n\n****************************************************" + '\x1b[0m' + "\n")
+print('\x1b[0;36;40m' + f"Welcome {player.name}" + '\x1b[0m' + "\n")
 
 # Write a loop that:
 #
@@ -49,3 +55,60 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+cmds = [
+    "n", "s", "e", "w", 
+    "N", "S", "E", "W",
+    "North", "South", "East", "West",
+    "north", "south", "east", "west"]
+
+def direction(player, cmd):
+    if cmd == "n" or cmd == "N" or cmd == "North" or cmd == "north":
+        if player.current_room.n_to:
+            dir = "Go North!"
+            player.current_room = player.current_room.n_to
+        else:
+            return print('\x1b[0;33;40m' +"I'm sorry you can't go North." + '\x1b[0m')
+    elif cmd == "s" or cmd == "S" or cmd == "South" or cmd == "south":
+        if player.current_room.s_to:
+            dir = "Go South!"
+            player.current_room = player.current_room.s_to
+        else:
+            return print('\x1b[0;33;40m' + "I'm sorry you can't go South." + '\x1b[0m')
+    elif cmd == "e" or cmd == "E" or cmd == "East" or cmd == "east":
+        if player.current_room.e_to:
+            dir = "Go East!"
+            player.current_room = player.current_room.e_to
+        else:
+            return print('\x1b[0;33;40m' + "I'm sorry you can't go East." + '\x1b[0m')
+    elif cmd == "w" or cmd == "W" or cmd == "West" or cmd == "west":
+        if player.current_room.w_to:
+            dir = "Go West!"
+            player.current_room = player.current_room.w_to
+        else:
+            return print('\x1b[0;33;40m' + "I'm sorry you can't go West." + '\x1b[0m')
+    else:
+        return print('\x1b[0;33;40m' + "I did not understand that command" + '\x1b[0m')
+    return print('\x1b[0;33;40m' + dir + '\x1b[0m' + "\n")
+
+
+while True:
+        print(art[player.current_room.name])
+        print('\x1b[1;34;40m' + f"{player.current_room.name}" + '\x1b[0m')
+        print('\x1b[1;34;40m' + f"{player.current_room.description}" + '\x1b[0m' + "\n")
+        print(
+            '\x1b[1;31;40m' +
+            "Exits are:",
+            'North' if player.current_room.n_to else '',
+            'South' if player.current_room.s_to else '',
+            'East' if player.current_room.e_to else '',
+            'West' if player.current_room.w_to else '' + '\x1b[0m')
+        print('\x1b[0;36;40m' + "****************************************************" + '\x1b[0m')
+        cmd = input('\x1b[1;33;40m' + "Please input command: " + '\x1b[0m')
+        if cmd == "q":
+            break
+        else:
+            direction(player, cmd)
+
+print("Thank you for playing")
